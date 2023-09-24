fa447012a0cb13eaf1516f0746b56f96bbdf77c6
Alex Korintely <mimidor204@gmail.com>
2023-09-25 02:49:19 +0300

    Попытка запуска сайта 15

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
